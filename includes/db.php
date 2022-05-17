9895a144e12a79e0a8cb2e698a0cdba04fb463b2
Michel Bittencourt <59713262+michel-bittencourt@users.noreply.github.com>
2022-05-17 16:54:37 -0300

    at

--- includes/db.php
+++ includes/db.php
@@ -1,14 +1,14 @@
 <?php
 
-	//$host 	= "localhost";
-	//$user 	= "root";
-	//$pass 	= "";
-	//$banco 	= "eldorado";
+	$host 	= "localhost";
+	$user 	= "root";
+	$pass 	= "";
+	$banco 	= "eldorado";
 	
-	$host 	= "mysql.tecnobem.com.br";
-	$user 	= "tecnobem01";
-	$pass 	= "Dti2021";
-	$banco 	= "tecnobem01";
+	//$host 	= "mysql.tecnobem.com.br";
+	//$user 	= "tecnobem01";
+	//$pass 	= "Dti2021";
+	//$banco 	= "tecnobem01";
 
 	$conn 	= mysqli_connect($host, $user, $pass) or die (mysqli_error());
 	mysqli_select_db($conn, $banco) or die (mysql_error());
